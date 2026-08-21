4edfd29fed588d0f106051a3fff250c0802ee27b
Luiz Andre Pego <luizandreafonsopego@gmail.com>
2026-08-21 12:24:12 -0300

    Aula 2

---
+++ Aula 2/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String[] args) {
+        int aux = 0, flag = 0;
+        for(int i = 1; i < 1000000; i++)
+        {
+            aux = 0;
+            if(flag == 100)
+            {
+                break;
+            }
+            for(int j = 1; j < i; j ++)
+            {
+                if(i%j==0)
+                {
+                    aux++;
+                }
+            }
+            if(aux == 1)
+            {
+                System.out.println("" + i);
+                flag++;
+            }
+        }
+    }
+}
